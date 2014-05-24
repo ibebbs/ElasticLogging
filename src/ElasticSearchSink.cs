62078dd783b933593428ed131b85a2f6cdb1be68
ibebbs <github@bebbs.co.uk>
2014-05-24 08:49:25 +0100

    Initial commit

---
+++ src/ElasticSearchSink.cs
@@ -0,0 +1,82 @@
+﻿using Microsoft.Practices.EnterpriseLibrary.SemanticLogging;
+using Microsoft.Practices.EnterpriseLibrary.SemanticLogging.Formatters;
+using PlainElastic.Net;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ElasticLogging
+{
+    public sealed class ElasticSearchSink : IObserver<EventEntry>
+    {
+        private const string DefaultDateFormat = "yyyy.MM.dd";
+        private const string DefaultIndex = "elasticsink";
+
+        private readonly string _connectionString;
+        private readonly string _index;
+        private readonly bool _appendDate;
+        private readonly string _dateFormat;
+        private readonly IEventTextFormatter _formatter;
+
+        public ElasticSearchSink(string connectionString, string index, bool appendDate, string dateFormat, IEventTextFormatter formatter)
+        {
+            if (string.IsNullOrEmpty(connectionString)) throw new ArgumentException("A ConnectionString must be specified", "connectionString");
+                
+            _connectionString = connectionString;
+            _index = string.IsNullOrWhiteSpace(index) ? DefaultIndex : index.ToLower();
+            _appendDate = appendDate;
+            _dateFormat = string.IsNullOrWhiteSpace(dateFormat) ? DefaultDateFormat : dateFormat;
+
+            _formatter = formatter ?? new EventTextFormatter();
+        }
+
+        public ElasticSearchSink(string connectionString, string index, bool appendDate, IEventTextFormatter formatter) : this(connectionString, index, appendDate, DefaultDateFormat, formatter) { }
+
+        public ElasticSearchSink(string connectionString, string index) : this(connectionString, index, false, new EventTextFormatter()) { }
+
+        private string GetIndex(EventEntry entry)
+        {
+            return string.Format("{0}{1}", _index, _appendDate ? entry.Timestamp.ToString(_dateFormat) : null);
+        }
+
+        private void Post(EventEntry entry)
+        {
+            try
+            {
+                ElasticConnection elasticConnection = new ElasticConnection(_connectionString);
+
+                string key = Guid.NewGuid().ToString();
+                string body = entry.ToJson();
+
+                var command = Commands.Index(GetIndex(entry), entry.Schema.ProviderName, key);
+                var result = elasticConnection.Put(command, body);
+            }
+            catch (Exception error)
+            {
+                // Do nothing for now
+                Console.WriteLine(error.ToString());
+            }
+        }
+
+        public void OnCompleted()
+        {
+            // Do nothing
+        }
+
+        public void OnError(Exception error)
+        {
+            // Do nothing
+        }
+
+        public void OnNext(EventEntry entry)
+        {
+            if (entry != null)
+            {
+                Post(entry);
+            }
+        }
+    }
+}
---
+++ src/JsonFactory.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.Practices.EnterpriseLibrary.SemanticLogging;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ElasticLogging
+{
+    public static class JsonFactory
+    {
+        private static JObject Payload(this EventEntry eventEntry)
+        {
+            JObject jo = new JObject();
+
+            for (int count = 0; count < eventEntry.Schema.Payload.Length; count++)
+            {
+                jo[eventEntry.Schema.Payload[count]] = JToken.FromObject(eventEntry.Payload[count]);
+            }
+
+            return jo;
+        }
+
+        public static string ToJson(this EventEntry eventEntry)
+        {
+            JObject jo = new JObject();
+            jo["EventId"] = eventEntry.EventId;
+            jo["EventName"] = eventEntry.Schema.EventName;
+            jo["ProviderId"] = eventEntry.ProviderId;
+            jo["ProviderName"] = eventEntry.Schema.ProviderName;
+            jo["TaskName"] = eventEntry.Schema.TaskName;
+            jo["OpCodeName"] = eventEntry.Schema.OpcodeName;
+            jo["KeywordsDescription"] = eventEntry.Schema.KeywordsDescription;
+            jo["FormattedMessage"] = eventEntry.FormattedMessage;
+            jo["Payload"] = eventEntry.Payload();
+            jo["Timestamp"] = eventEntry.Timestamp;
+
+            return jo.ToString();
+        }
+    }
+}
